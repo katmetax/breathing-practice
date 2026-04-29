a6c5d9173ae9c9a27af4dd95b7909c6c3b9ec2a1
Kat Metaxopoulou <kat.metax@and.digital>
2026-04-29 12:30:33 +0200

    Make into PWA

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,32 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Icons from 'unplugin-icons/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), Icons({ compiler: 'vue3' }), basicSsl()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    Icons({ compiler: 'vue3' }),
+    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Breathing Practice',
+        short_name: 'Breathe',
+        theme_color: '#ffffff',
+        icons: [
+          { src: 'icons/pwa-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/pwa-512.png', sizes: '512x512', type: 'image/png' },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
